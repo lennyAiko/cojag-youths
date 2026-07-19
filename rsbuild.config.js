edecf7d27cea68bacbc4de1c2c863aff430e2187
Lennox Charles <lennox.charles@tolaram.com>
2026-07-19 16:12:58 +0100

    initial commit

---
+++ rsbuild.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [
+    pluginReact({
+      reactCompiler: true,
+    }),
+    pluginTailwindcss(),
+  ],
+});
